3b94c444d1e5cae9a9f539b92336f42866ee2086
Alex Berger <alexbigkid@users.noreply.github.com>
2020-07-26 11:01:30 -0700

    F adjusted the git alias commands

--- macBin/setGitAliases.sh
+++ macBin/setGitAliases.sh
@@ -5,10 +5,10 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.unstage reset HEAD --
-git config --global alias.last log -1 HEAD
-git config --global alias.cob checkout -b
-git config --global alias.alias config --get-regexp ^alias\.
-git config --global alias.rtl reset --hard HEAD
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.last "log -1 HEAD"
+git config --global alias.cob "checkout -b"
+git config --global alias.alias "config --get-regexp ^alias\."
+git config --global alias.rtl "reset --hard HEAD"
 
 exit 0
