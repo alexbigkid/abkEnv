ce919f4ca83a8876bb3923d8c253d0e7e706ad27
Alex Berger <Alex.Berger@hunterindustries.com>
2020-10-08 23:29:02 -0700

    F windows updated git aliases

--- winBin/setGitAliases.ps1
+++ winBin/setGitAliases.ps1
@@ -10,6 +10,11 @@ git config --global alias.last "log -1 HEAD"
 git config --global alias.cob "checkout -b"
 git config --global alias.alias "config --get-regexp ^alias\."
 git config --global alias.rtl "reset --hard HEAD"
-
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.logg 'log --pretty=format:"%h %s" --graph'
+git config --global alias.today "log --pretty=format:'%Cred%h %Cgreen%cd%Creset | %s%C(auto)%d %Cgreen[%an]%Creset' --date=local --since=midnight"
+git config --global alias.dbranch "!git checkout master && git branch -D $1 && git push origin --delete $1"
+git config --global alias.brr "!git fetch -p && git branch -r"
+git config --global alias.drbranch "!git checkout master && git push origin --delete $1"
 Write-Host "<-" $MyInvocation.MyCommand.Name -ForegroundColor Yellow
 exit 0
