5d940c77e69888099633dc054cb6e8c44ec90afe
Alex Berger <alexbigkid@users.noreply.github.com>
2020-10-08 17:14:25 -0700

    F updated git aliases

--- macBin/setGitAliases.sh
+++ macBin/setGitAliases.sh
@@ -5,10 +5,12 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.unstage reset HEAD --
-git config --global alias.last log -1 HEAD
-git config --global alias.cob checkout -b
-git config --global alias.alias config --get-regexp ^alias\.
-git config --global alias.rtl reset --hard HEAD
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.last "log -1 HEAD"
+git config --global alias.cob "checkout -b"
+git config --global alias.alias "config --get-regexp ^alias\."
+git config --global alias.rtl "reset --hard HEAD"
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.today "log --pretty=format:'%Cred%h %Cgreen%cd%Creset | %s%C(auto)%d %Cgreen[%an]%Creset' --date=local --since=midnight"
 
 exit 0
