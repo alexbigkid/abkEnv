7cec19eaf8b68e263b662762b3af98f66e91722e
Alex Berger <alexbigkid@users.noreply.github.com>
2020-10-08 17:33:49 -0700

    F updated git aliases

--- macBin/setGitAliases.sh
+++ macBin/setGitAliases.sh
@@ -13,4 +13,9 @@ git config --global alias.rtl "reset --hard HEAD"
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.today "log --pretty=format:'%Cred%h %Cgreen%cd%Creset | %s%C(auto)%d %Cgreen[%an]%Creset' --date=local --since=midnight"
 
+git config --global alias.logg 'log --pretty=format:"%h %s" --graph'
+git config --global alias.dbranch "!git checkout master && git branch -D $1 && git push origin --delete $1"
+git config --global alias.brr "!git fetch -p && git branch -r"
+git config --global alias.drbranch "!git checkout master && git push origin --delete $1"
+
 exit 0
