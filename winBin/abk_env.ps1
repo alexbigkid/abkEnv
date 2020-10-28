3ae69daf06a5815c4daf64d96fafa318d7b66f06
Alex Berger <Alex.Berger@hunterindustries.com>
2020-10-27 23:27:31 -0700

    e removing not working code

--- winBin/abk_env.ps1
+++ winBin/abk_env.ps1
@@ -3,8 +3,6 @@ $ErrorActionPreference = "Stop"
 
 $ABK_BIN_DIR="$HOME\abkBin"
 
-function myFunctions {gci function: }
-
 Import-Module "$ABK_BIN_DIR\Modules\abk-lib"
 Import-Module "$ABK_BIN_DIR\Modules\abk-aliases"
 $EXIT_CODE=$ERROR_CODE_SUCCESS
