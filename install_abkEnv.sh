f088445602082b6b9d3217420e1155b4788c5f9c
Alex Berger <alexbigkid@users.noreply.github.com>
2020-12-12 08:13:14 -0800

    F introduced filtered level tracing

--- install_abkEnv.sh
+++ install_abkEnv.sh
@@ -15,7 +15,7 @@ function PrintUsageAndExitWithCode() {
 }
 
 function __install_abkEnv_uninstall_old() {
-    echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
     if [ -d "$HOME/env" ]; then
         # looks like legacy installation, deleting previous version
         local LCL_UNINSTALL_1_0_0="macInstallVersions/uninstall_abkEnv_1_0_0.sh"
@@ -24,19 +24,12 @@ function __install_abkEnv_uninstall_old() {
             uninstall_abkEnv_1_0_0_main
         fi
     fi
-    echo "<- ${FUNCNAME[0]} (0)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (0)"
 }
 
 
 function __install_abkEnv_bash() {
-    echo "-> ${FUNCNAME[0]} ($@)"
-    echo "   [BIN_DIR           = $BIN_DIR]"
-    echo "   [HOME_BIN_DIR      = $HOME_BIN_DIR]"
-    echo "   [SH_BIN_DIR        = $SH_BIN_DIR]"
-    echo "   [SH_PACKAGES_DIR   = $SH_PACKAGES_DIR]"
-    echo "   [ABK_ENV_FILE      = $ABK_ENV_FILE]"
-    echo "   [HOME              = $HOME]"
-    echo
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
 
     # uninstall old installations
     __install_abkEnv_uninstall_old
@@ -65,41 +58,44 @@ function __install_abkEnv_bash() {
 
     source $HOME/$CONFIG_FILE_BASH
 
-    echo "<- ${FUNCNAME[0]} (0)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (0)"
     return 0
 }
 
 function __install_abkEnv_zsh() {
-    echo "-> ${FUNCNAME[0]} ($@)"
-    echo "<- ${FUNCNAME[0]} (0)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (0)"
 }
 
 #---------------------------
 # main
 #---------------------------
 function install_abkEnv_main() {
-    # echo "-> ${FUNCNAME[0]} ($@)"
-    # vars
     local LCL_RED='\033[0;31m'
     local LCL_NC='\033[0m' # No Color
-    declare -a LCL_ABK_SUPPORTED_SHELLS=(
-        "bash"
-        "zsh"
-    )
     local LCL_ABK_SCRIPT_TO_EXECUTE="__install_abkEnv"
     local LCL_ABK_LIB_FILE="macBinBash/AbkLib.sh"
     [ -f $LCL_ABK_LIB_FILE ] && source $LCL_ABK_LIB_FILE || PrintUsageAndExitWithCode 1 "${LCL_RED}ERROR:${LCL_NC} $LCL_ABK_LIB_FILE could not be found."
 
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [BIN_DIR           = $BIN_DIR]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [HOME_BIN_DIR      = $HOME_BIN_DIR]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [SH_BIN_DIR        = $SH_BIN_DIR]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [SH_PACKAGES_DIR   = $SH_PACKAGES_DIR]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [ABK_ENV_FILE      = $ABK_ENV_FILE]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [HOME              = $HOME]"
+    [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo
+
     # Is parameter --help?
     [ "$#" -eq 1 ] && [ "$1" == "--help" ] && PrintUsageAndExitWithCode $ERROR_CODE_SUCCESS
     # Is number of parameters ok
-    [ "$#" -ne 0 ] && PrintUsageAndExitWithCode $ERROR_CODE_GENERAL_ERROR "${LCL_RED}ERROR: invalid number of parameters${NC}"
+    [ "$#" -ne 0 ] && PrintUsageAndExitWithCode $ERROR_CODE_GENERAL_ERROR "${RED}ERROR: invalid number of parameters${NC}"
     # is $SHELL supported
-    AbkLib_IsStringInArray $ABK_SHELL "${LCL_ABK_SUPPORTED_SHELLS[@]}" || PrintUsageAndExitWithCode $? "${LCL_RED}ERROR:${LCL_NC} $ABK_SHELL is not supported.\n Please consider using one of those shells: ${LCL_ABK_SUPPORTED_SHELLS[*]}"
+    AbkLib_IsStringInArray $ABK_SHELL "${ABK_SUPPORTED_SHELLS[@]}" || PrintUsageAndExitWithCode $? "${RED}ERROR:${NC} $ABK_SHELL is not supported.\nPlease consider using one of those shells: ${ABK_SUPPORTED_SHELLS[*]}"
     # run shell specific install
-    __install_abkEnv_${ABK_SHELL} || PrintUsageAndExitWithCode $? "${RED}ERROR:${NC} __install_abkEnv_${ABK_SHELL} failed"
-    echo "<- ${FUNCNAME[0]} (0)"
-exit 0
+    ${LCL_ABK_SCRIPT_TO_EXECUTE}_${ABK_SHELL} || PrintUsageAndExitWithCode $? "${RED}ERROR:${NC} ${LCL_ABK_SCRIPT_TO_EXECUTE}_${ABK_SHELL} failed"
+
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (0)"
     return 0
 }
 
@@ -107,6 +103,7 @@ echo ""
 echo "-> $0 ($@)"
 
 install_abkEnv_main $@
+LCL_EXIT_CODE=$?
 
 echo "<- $0 (0)"
-exit 0
+exit $LCL_EXIT_CODE
--- macBinBash/AbkLib.sh
+++ macBinBash/AbkLib.sh
@@ -7,6 +7,15 @@
 declare -r TRUE=0
 declare -r FALSE=1
 
+ABK_NO_TRACE=0
+ABK_CRITICAL_TRACE=1
+ABK_FUNCTION_TRACE=2
+ABK_INFO_TRACE=3
+ABK_DETAILED_TRACE=4
+ABK_ALL_TRACE=5
+ABK_TRACE=$ABK_NO_TRACE
+
+
 # exit error codes
 ERROR_CODE_SUCCESS=0
 ERROR_CODE_GENERAL_ERROR=1
@@ -15,22 +24,26 @@ ERROR_CODE=$ERROR_CODE_SUCCESS
 
 ABK_SHELL="${SHELL##*/}"
 ABK_SHELL_1ST_UPPER=$(tr '[:lower:]' '[:upper:]' <<< ${ABK_SHELL:0:1})${ABK_SHELL:1}
-# echo "ABK_SHELL_1ST_UPPER = $ABK_SHELL_1ST_UPPER"
+[ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "ABK_SHELL_1ST_UPPER = $ABK_SHELL_1ST_UPPER"
 BIN_DIR="abkBin$ABK_SHELL_1ST_UPPER"
 SH_BIN_DIR="macBin$ABK_SHELL_1ST_UPPER"
 HOME_BIN_DIR="$HOME/$BIN_DIR"
 SH_PACKAGES_DIR="macPackages"
 
+declare -a ABK_SUPPORTED_SHELLS=(
+    "bash"
+    "zsh"
+)
+
 CONFIG_FILE_BASH=".bash_profile"
 CONFIG_FILE_ZSH=".zshrc"
 
-# abkEnv bash profile file names
+# abkEnv shell profile file names
 ABK_LIB_FILE_DIR=$(dirname "$BASH_SOURCE")
-echo "ABK_LIB_FILE_DIR = $ABK_LIB_FILE_DIR"
-echo "PWD = $PWD"
 ABK_ENV_FILE="$PWD/$ABK_LIB_FILE_DIR/env/abk_env.env"
-echo "ABK_ENV_FILE = $ABK_ENV_FILE"
-
+[ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "ABK_LIB_FILE_DIR = $ABK_LIB_FILE_DIR"
+[ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "PWD = $PWD"
+[ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "ABK_ENV_FILE = $ABK_ENV_FILE"
 
 #---------------------------
 # color definitions
@@ -49,7 +62,7 @@ ABK_ENV_END="# <<<<<< DO_NOT_REMOVE <<<<<< ABK_ENV <<<< END"
 # functions
 #---------------------------
 function AbkLib_AddEnvironmentSettings() {
-    echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
     local LCL_FILE_TO_ADD_CONTENT_TO=$1
     local LCL_SETTING_FILE_TO_ADD=$2
     local LCL_RESULT=$FALSE
@@ -58,9 +71,9 @@ function AbkLib_AddEnvironmentSettings() {
         LCL_RESULT=$TRUE
 
         if grep -q $LCL_SETTING_FILE_TO_ADD $LCL_FILE_TO_ADD_CONTENT_TO; then
-            echo "   [ABK environment already added. Nothing to do here.]"
+            [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [ABK environment already added. Nothing to do here.]"
         else
-            echo "   [adding ABK environment ...]"
+            [ "$ABK_TRACE" -ge "$ABK_INFO_TRACE" ] && echo "   [adding ABK environment ...]"
             cat >>$LCL_FILE_TO_ADD_CONTENT_TO <<-TEXT_TO_ADD
 
 $ABK_ENV_BEGIN
@@ -75,25 +88,25 @@ TEXT_TO_ADD
         echo -e "${RED}   One or both files do not exist: $LCL_FILE_TO_ADD_CONTENT_TO, $LCL_SETTING_FILE_TO_ADD${NC}"
     fi
 
-    echo "<- ${FUNCNAME[0]} ($LCL_RESULT)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} ($LCL_RESULT)"
     return $LCL_RESULT
 }
 
 function AbkLib_IsParameterHelp() {
-    echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
     local NUMBER_OF_PARAMETERS=$1
     local PARAMETER=$2
     if [[ $NUMBER_OF_PARAMETERS -eq 1 && $PARAMETER == "--help" ]]; then
-        echo "<- ${FUNCNAME[0]} (TRUE)"
+        [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (TRUE)"
         return $TRUE
     else
-        echo "<- ${FUNCNAME[0]} (FALSE)"
+        [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} (FALSE)"
         return $FALSE
     fi
 }
 
 function AbkLib_IsStringInArray() {
-    echo "-> ${FUNCNAME[0]} ($@)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "-> ${FUNCNAME[0]} ($@)"
     local LCL_STRING_TO_SEARCH_FOR=$1
     shift
     local LCL_ARRAY_TO_SEARCH_IN=("$@")
@@ -106,6 +119,6 @@ function AbkLib_IsStringInArray() {
         fi
     done
 
-    echo "<- ${FUNCNAME[0]} ($LCL_MATCH_FOUND)"
+    [ "$ABK_TRACE" -ge "$ABK_FUNCTION_TRACE" ] && echo "<- ${FUNCNAME[0]} ($LCL_MATCH_FOUND)"
     return $LCL_MATCH_FOUND
 }
