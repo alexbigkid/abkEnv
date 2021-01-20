841bba9a3dea4e64dfb7cf0542e7099aca1baf4d
Alex Berger <alexbigkid@users.noreply.github.com>
2021-01-19 16:30:01 -0800

    B winBin: corrected the year format remoteWorkDate F winBin: added copying to clipboard message to remoteWorkDate

--- winBin/remoteWorkDate.ps1
+++ winBin/remoteWorkDate.ps1
@@ -1,3 +1,3 @@
-$ABK_DATE_STR = Get-Date -UFormat "%F week:%V %a"
+$ABK_DATE_STR = Get-Date -UFormat "%Y-%m-%d week:%V %a"
+Write-Host "Copying to clipboard the date: $ABK_DATE_STR"
 Write-Output $ABK_DATE_STR | Set-Clipboard
-Write-Host $ABK_DATE_STR
