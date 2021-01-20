9defa51c121510d9aeb84e34861a2c206b15c588
Alex Berger <alexbigkid@users.noreply.github.com>
2021-01-19 18:04:48 -0800

    F added ABK_UNIX_TYPE environment variable

--- macBin/newGuid.zsh
+++ macBin/newGuid.zsh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 
 NEW_GUID=$(uuidgen | tr "[:upper:]" "[:lower:]")
-echo $NEW_GUID | tr -d '\n' | pbcopy
-echo $NEW_GUID
+if [ "$ABK_UNIX_TYPE" = "mac" ]; then
+    echo "Copying to clipboard new GUID: $NEW_GUID"
+    echo $NEW_GUID | tr -d '\n' | pbcopy
+else
+    if [ "$(command -v xclip)" == "" ]; then
+        echo "New GUID: $NEW_GUID"
+    else
+        echo "Copying to clipboard new GUID: $NEW_GUID"
+        echo $NEW_GUID | xclip
+        echo $NEW_GUID | xclip -sel clip
+    fi
+fi
 
 exit 0
--- macBin/remoteWorkDate.zsh
+++ macBin/remoteWorkDate.zsh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 
 ABK_DATE_STR=$(date "+%F week:%V %a")
-echo $ABK_DATE_STR | tr -d '\n' | pbcopy
-echo $ABK_DATE_STR
+if [ "$ABK_UNIX_TYPE" = "mac" ]; then
+    echo "Copying to clipboard the date: $ABK_DATE_STR"
+    echo $ABK_DATE_STR | tr -d '\n' | pbcopy
+else
+    if [ "$(command -v xclip)" == "" ]; then
+        echo $ABK_DATE_STR
+    else
+        echo "Copying to clipboard the date: $ABK_DATE_STR"
+        echo $ABK_DATE_STR | xclip
+        echo $ABK_DATE_STR | xclip -sel clip
+    fi
+fi
 
 exit 0
