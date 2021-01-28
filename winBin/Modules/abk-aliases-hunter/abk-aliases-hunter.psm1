de8e1af94de4232e9621ec1b1279335e0fa49507
Alex Berger <alexbigkid@users.noreply.github.com>
2021-01-28 10:06:03 -0800

    F winBin: updated directories aliases

--- winBin/Modules/abk-aliases-hunter/abk-aliases-hunter.psm1
+++ winBin/Modules/abk-aliases-hunter/abk-aliases-hunter.psm1
@@ -2,25 +2,26 @@
 
 function cdcat { Set-Location -Path C:\Git\mob\webcentral\Centralus-AcceptanceTest }
 function cdcspt { Set-Location -Path C:\Git\mob\webcentral\Centralus-SupportPageTerraform }
+function cdcas { Set-Location -Path C:\Git\mob\webcentral\Centralus.ApiServer }
+function cdcc { Set-Location -Path C:\Git\mob\webcentral\Centralus.Common }
+function cdcct { Set-Location -Path C:\Git\mob\webcentral\Centralus.ControllerTranslation }
 function cdcf { Set-Location -Path C:\Git\mob\webcentral\Centralus.Firmware }
 function cdci { Set-Location -Path C:\Git\mob\webcentral\Centralus.Infrastructure }
 function cdcia { Set-Location -Path C:\Git\mob\webcentral\Centralus.InfrastructureAzure }
-function cdcp { Set-Location -Path C:\Git\mob\webcentral\Centralus.ProC2 }
 function cdcl { Set-Location -Path C:\Git\mob\webcentral\Centralus.Lambdas }
+function cdcp { Set-Location -Path C:\Git\mob\webcentral\Centralus.ProC2 }
+function cdcrs { Set-Location -Path C:\Git\mob\webcentral\Centralus.RetryServer }
+function cdcss { Set-Location -Path C:\Git\mob\webcentral\Centralus.SecurityServer }
+function cdcs { Set-Location -Path C:\Git\mob\webcentral\Centralus.Support }
+function cdcu { Set-Location -Path C:\Git\mob\webcentral\Centralus.Utilities }
+function cdcws { Set-Location -Path C:\Git\mob\webcentral\Centralus.WebServer }
 function cdhim { Set-Location -Path C:\Git\mob\webcentral\HunterInfrastructureModules }
-function cdwas { Set-Location -Path C:\Git\mob\webcentral\WebCentral.ApiServer }
 function cdwb { Set-Location -Path C:\Git\mob\webcentral\WebCentral.BDD }
-function cdwc { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Common }
 function cdwi { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Infrastructure }
 function cdwld { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Lambda.DeviceResponseToRDS }
 function cdwlf { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Lambda.FirmwareUpdate }
 function cdwlll { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Lambda.LambdaLayers }
 function cdwll { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Lambdas }
-function cdwrs { Set-Location -Path C:\Git\mob\webcentral\WebCentral.RetryServer }
-function cdwss { Set-Location -Path C:\Git\mob\webcentral\WebCentral.SecurityServer }
-function cdws { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Support }
-function cdwu { Set-Location -Path C:\Git\mob\webcentral\WebCentral.Utilities }
-function cdwws { Set-Location -Path C:\Git\mob\webcentral\WebCentral.WebServer }
 
 Export-ModuleMember -Function *
 Export-ModuleMember -Variable *
